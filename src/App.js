1cd04ac0b609d495209df9f0c0ab0c23098a57e9
Artem <oleksienkoartem@gmail.com>
2020-12-22 17:31:45 +0200

    add react-bootstrap, node-sass, styles for Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginContainer from "./components/LoginContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {Field, Form, Formik} from "formik";
+import {Button} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Login = ({initialValues, validationSchema, submitButtonText, onSubmit}) => {
 
@@ -11,16 +14,42 @@ const Login = ({initialValues, validationSchema, submitButtonText, onSubmit}) =>
                 validationSchema={validationSchema}
             >
                 {({errors, touched}) => (
-                    <Form>
-                        <label htmlFor="email" className="formTab__label">Email:</label>
-                        <Field name="email" type="email"/>
-                        {errors.email && touched.email ? <div>{errors.email}</div> : null}
+                    <Row>
+                        <Col md={{span: 6, offset: 3}}>
+                            <Form className="form">
+                                <Row>
+                                    <Col md={2}>
+                                        <label htmlFor="email" className="form__label">Email:</label>
+                                    </Col>
+                                    <Col md={10}>
+                                        <div className="form__field">
+                                        <Field name="email" type="email" className="form__field_input"/>
+                                        {errors.email && touched.email
+                                            ? <div className="form__error">{errors.email}</div>
+                                            : null}
+                                        </div>
+                                    </Col>
 
-                        <label htmlFor="password" className="formTab__label">Password:</label>
-                        <Field name="password" type="password"/>
-                        {errors.password && touched.password ? <div>{errors.password}</div> : null}
-                        <button type="submit" className="btn">{submitButtonText}</button>
-                    </Form>
+                                </Row>
+
+                                <Row>
+                                    <Col md={2}>
+                                        <label htmlFor="password" className="formTab__label">Password:</label>
+                                    </Col>
+                                    <Col md={10}>
+                                        <div className="form__field">
+                                        <Field name="password" type="password" className="form__field_input" />
+                                        {errors.password && touched.password
+                                            ? <div className="form__error">{errors.password}</div>
+                                            : null}
+                                        </div>
+                                    </Col>
+
+                                </Row>
+                                <Button variant="success" size="lg" type="submit" className="form_btn">{submitButtonText}</Button>
+                            </Form>
+                        </Col>
+                    </Row>
                 )}
             </Formik>
         </>
